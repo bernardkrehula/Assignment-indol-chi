af6bb10519a5ef27ce46e63b6dea3933ae95ea2a
bernardkrehula <krehula.bernard456@gmail.com>
2025-02-05 21:46:49 +0100

    Limited to only 3 boxes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let main = document.querySelector('.main');
 let btn = document.querySelector('.main button');
-let listBtn = document.querySelectorAll('.main li button');
+let listBtn = document.querySelectorAll('.main ul li button');
 
 let number = 0;
 
@@ -11,45 +11,46 @@ btn.addEventListener('click', () => {
     createNewBox();
 })
 
+
 function createNewBox() {
     let createList = document.createElement('li');
     main.appendChild(createList);
-
-    for(i = 0; i < 3; i++) {
-        let createBtn = document.createElement('button');
-        createList.appendChild(createBtn);
+    listBtn = document.querySelectorAll('.main li button');
+    if(listBtn.length < 9) {
+        for(i = 0; i < 3; i++) {
+            let createBtn = document.createElement('button');
+            createList.appendChild(createBtn);
+        }
     }
     defineButtons();
     activatePlus();
     activteMinus();
 }
+
 function defineButtons() {
     listBtn = document.querySelectorAll('.main li button');
     listBtn[0].innerHTML = '-';
     listBtn[1].innerHTML = 0;
     listBtn[2].innerHTML = '+';
+    //console.log(listBtn)
 }
 
 function activatePlus() {
     plus = listBtn[2];
     plus.addEventListener('click', () => {
-        addValue();
+        if(number >= 0){
+            number += 1;
+            console.log(number)
+        }
     })
 }
-function addValue() {
-    if(number >= 0){
-        number += 1;
-    }
-}
+   
 function activteMinus() {
     minus = listBtn[0];
     minus.addEventListener('click', () => {
-        subtractValue();
+        if(number >= 0) {
+            number -= 1;
+            console.log(number)
+        }
     })
-}
-
-function subtractValue() {
-    if(number <= 0) {
-        number -= 1;
-    }
 }
