b9ca32670d55537de90addc48b4ca3072b57e94b
bernardkrehula <krehula.bernard456@gmail.com>
2025-02-09 15:06:09 +0100

    Added functionality to box and marbles count

--- script.js
+++ script.js
@@ -1,38 +1,28 @@
 let main = document.querySelector('.main');
 let btn = document.querySelector('.main button');
-let list = document.querySelectorAll('.main li');
+let list = document.querySelectorAll('.menu li');
 let minus = document.querySelectorAll('.minus');
 let resultNumber = document.querySelector('.number');
 let plus = document.querySelector('.plus');
 let menu = document.querySelector('.menu');
-/*
-Napravi array allCounters = []
-Na click dugmeta addNewBox napravi objekat = {id: 1, counter: 0}
-Gurni taj objekat u array  allCounters
-Napravi funkciju createBoxes
-Ona za svaki objekat u arrayu allCounters pravi kutiju
-
-
-Obrati paznju da na svaki box zakacis id countera koji joj odgovara
-Sad svaka kutija ima svoj id koji odgovara id countera u arrayu allCounters
-Na click plusa ili minusa nades tu kutiju i procitas njen id (pogledaj closest dom metodu)
-Uz pomocu tog id nades counter u arrayu allCounters
-Povecas ili smanjis njegov counter
-Updejtujes textContent kutije
-*/ 
+let marbles = document.querySelector('.marbles-count');
+let boxes = document.querySelector('.box-count');
 
 let allCounters = [];
 
+let boxesCount = 0;
+
+let marblesCount = 0;
 
 function pushObjects(box) {
     allCounters.push(box)
 }
 
-
 btn.addEventListener('click', () => {
     const counter = { id: crypto.randomUUID(), value: 0}
     pushObjects(counter);
-    createNewBox(counter) 
+    createNewBox(counter);
+    countBoxes()
 })
 function createNewBox(counter) {
     let listElement = document.createElement('li');
@@ -46,24 +36,30 @@ function createNewBox(counter) {
     createNumber.innerHTML = counter.value;
     createPlus.innerHTML = '+';
 
+    marbles = document.querySelector('.marbles-count');
+
     createPlus.addEventListener('click', function (event) {
         let id = event.target.closest('li').id;
-
         const currentCounter = allCounters.find((box) => {
             return box.id == id;
         })
         currentCounter.value++;
         createNumber.textContent = currentCounter.value;
+
+        marblesCount++;
+        marbles.textContent = marblesCount;
         })
 
     createMinus.addEventListener('click', function(event) {
         let id = event.target.closest('li').id;
-
         const currentCounter = allCounters.find((box) => {
             return box.id == id;
         })
         currentCounter.value--;
         createNumber.textContent = currentCounter.value;
+
+        marblesCount--;
+        marbles.textContent = marblesCount;
     })
 
     createMinus.classList.add('minus');
@@ -75,4 +71,10 @@ function createNewBox(counter) {
     listElement.appendChild(createPlus);
 
     menu.appendChild(listElement);
+}
+
+function countBoxes() {
+    list = document.querySelectorAll('.menu li');
+    boxesCount = list.length;
+    boxes.textContent = boxesCount;
 }
